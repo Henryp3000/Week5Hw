2bc28aa0f5739d5fadb1b0fdb25580b53cd23f3a
Henry <Henrypereira30@gmail.com>
2022-09-27 09:21:55 -0400

    PhoneBook

---
+++ HomeWorkW4/src/main/java/Person.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Person {
+
+
+    public static void main(String[] args) {
+       // String[] MyPeople = new String [5];
+//        Person[0] = "Henry";
+//        Person[1] = "Nyshia";
+//        Person[2] = "Mike";
+//        Person[3] = "Amy";
+//        Person[4] = "Harlen";
+
+
+        ArrayList MyPeople = new ArrayList<>();
+        MyPeople.add("Henry");
+        MyPeople.add("Nyshia");
+        MyPeople.add("Mike");
+        MyPeople.add("Amy");
+        MyPeople.add("Harlen");
+        System.out.println(MyPeople);
+    }
+}
---
+++ HomeWorkW4/src/main/java/PhoneBook.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class PhoneBook {
+
+
+    public static void main(String[] args) {
+        /* Create a phonebook using hashmap to store Name and Number
+
+         */
+        HashMap< String, String > PhoneBook = new HashMap<String, String>();
+        PhoneBook.put("Henry", "3333333333");
+        PhoneBook.put("Nyshia", "3334443434");
+        PhoneBook.put("Mike", "2813308004");
+        PhoneBook.put("Amy", "1234567890");
+        System.out.println("");
+
+    }
+
+
+}
