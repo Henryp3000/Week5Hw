3a9f038cbf0f164ff29e1b8eca1d521c0738eb3d
Henry <Henrypereira30@gmail.com>
2022-10-13 15:37:20 -0400

    here

---
+++ Oct13HW/src/main/java/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+/*
+Create a Person class.
+The class should declare a final field named id of type long.
+The class should declare a field named name of type String.
+Person constructor should have a parameter of type long and String which sets the id and name field to the respective values.
+The class should define a getId() method which returns the Person object's id field.
+The class should define a getName() method which returns the Person object's name field.
+The class should define a setName() method which sets the Person object's name field.
+
+ */
+}
